ec87b65de6729fadd8579b83061d57c03cd70320
aswinshenoy <aswinshenoy65@gmail.com>
2019-02-06 23:13:36 +0530

    Added aliases

---
+++ aliases.sh
@@ -0,0 +1,11 @@
+# Package Installation
+alias install='sudo apt-get install'
+alias remove='sudo apt-get remove'
+alias update='sudo apt-get update'
+alias upgrade='sudo apt-get update && sudo apt-get upgrade'
+
+# Change Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
--- rashrc.sh
+++ rashrc.sh
@@ -2,3 +2,6 @@
 
 PS1="\[\e[90m\][\A] \[\e[1;34m\]\u\[\e[0m\]@\[\e[33m\]\h\[\e[0m\]: \[\e[1;32m\]\w \[\e[0m\]$ "
 export PS1
+
+DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
+source $DIRECTORY/aliases.sh
