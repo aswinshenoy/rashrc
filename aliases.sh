410cbeb1734231c296aac1323a12a89d51c2038a
aswinshenoy <aswinshenoy65@gmail.com>
2019-03-01 23:48:16 +0530

    add git shortcut

--- aliases.sh
+++ aliases.sh
@@ -9,3 +9,22 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
+# Git Shortcuts
+alias gs='git status '
+alias ga='git add '
+alias gaa='git add -A'
+alias gb='git branch '
+alias gbv='git branch -vv'
+alias gc='git commit'
+alias gcm='git commit -m'
+alias gd='git diff'
+alias go='git checkout '
+alias gco='git checkout '
+alias gl='git log --all --graph --decorate --oneline'
+alias p='git pull'
+alias gp='git push'
+alias gm='git merge'
+alias gr='git remote'
+
+
+
--- rashrc.sh
+++ rashrc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 source /usr/lib/git-core/git-sh-prompt
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWUNTRACKEDFILES=1
